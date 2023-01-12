dc6a3f3cc7fd312b634c49e3778363c84c3de09e
sooox0809 <sooox89@gmail.com>
2023-01-12 17:52:05 +0900

    subjects 03

--- day01.py
+++ day01.py
@@ -1,15 +1,19 @@
-# for countdown in 5, 4, 3, 2, 1, "hey!":
-#    print(countdown)
-# print (' 프로그램 종료')
+# 교재 딕셔너리 예제
 
-countdown_list = [5, 4, 3, 2, 1, "hey!"]
-for countdown in countdown_list:
-    print(countdown)
-print('프로그램 종료')
+subjects = {
+    '의사소통 영어' : 'A+', '오래된 미래' : 'B+', '양자역학' : 'A0'}
 
-countdown_list = [5, 4, 3, 2, 1, "hey!"]
-for countdown in countdown_list:
-    print(countdown)
-print(countdown_list[3])
-# 인덱스, 오프셋
+student = '김도훈'
+subject = '오래된 미래'
+print(student, '학생의', subject, '과목 성적은', subjects['오래된 미래'], '입니다')
 
+# 놓침,list
+
+# old style
+print("%s 학생의 %s 과목 성적은 %s입니다" % (student, subject, subjects[subject]))
+
+#modern style (format 함수)
+print("{2} 학생의 {1} 과목성적은 {0}입니다". format(student, subject, subjects[subject]))
+#print ("{} 학생의 {}
+#f string
+print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다')
