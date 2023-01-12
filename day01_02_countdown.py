2a636b5fba730ef942bfcb259dbf48ec3dc709e6
sooox0809 <sooox89@gmail.com>
2023-01-12 17:30:52 +0900

    temperature 01

---
+++ day01_02_countdown.py
@@ -0,0 +1,15 @@
+# for countdown in 5, 4, 3, 2, 1, "hey!":
+#    print(countdown)
+# print (' 프로그램 종료')
+
+countdown_list = [5, 4, 3, 2, 1, "hey!"]
+for countdown in countdown_list:
+    print(countdown)
+print('프로그램 종료')
+
+countdown_list = [5, 4, 3, 2, 1, "hey!"]
+for countdown in countdown_list:
+    print(countdown)
+print(countdown_list[3])
+# 인덱스, 오프셋
+
