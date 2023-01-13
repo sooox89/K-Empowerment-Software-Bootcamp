6dd1c698e96e81b2b38e9470c474cba8d4e23ee4
sooox0809 <sooox89@gmail.com>
2023-01-13 13:53:26 +0900

    day02 numbers

--- day02.py
+++ day02.py
@@ -7,17 +7,37 @@
 #math_values[0] = 9.99
 #print(f'원주율의 값은 {math_values[0]}이고 타입은 {type(math_values)}입니다')
 
-print(2**5)
-print(pow(2,5))
-print(divmod(9,5))
-print(type(divmod(9,5)))
-print(type(1,2))
-test = 1,2 #packing
-print(type(test))
-print(test)
-print(test[1])
-a, b, c = test #unpacking
-print(a)
-print(b)
+# print(2**5)
+#print(pow(2,5))
+#print(divmod(9,5))
+#print(type(divmod(9,5)))
+#print(type(1,2))
+#test = 1,2 #packing
+#print(type(test))
+#print(test)
+#print(test[1])
+#a, b, c = test #unpacking
+#print(a)
+#print(b)
+
+
+
+number = 0b10011010
+print(number)
+
+number = 0x9A
+print(number)
+
+number= 0o232
+print(number)
+
+number = 154
+print(bin(number))
+print(hex(number))
+print(oct(number))
+
+
+print(ord(" "))  ##ascii
+print(hex(print(ord(" "))))   ##
 
 
