a095eff3cc97bc61c61aeba8c8305da6dbe8e1c2
sooox0809 <sooox89@gmail.com>
2023-01-13 13:35:51 +0900

    day02 numbers

--- day02.py
+++ day02.py
@@ -1,6 +1,23 @@
-harry_porter = {'헤르미온느': '그리핀도르', '말포이': '슬리데린', '론' : '그리핀도르'}
-print(harry_porter)
-print(type(harry_porter))
+# harry_porter = {'헤르미온느': '그리핀도르', '말포이': '슬리데린', '론' : '그리핀도르'}
+# print(harry_porter)
+# print(type(harry_porter))
 
+#math_values = (3.14, 2.71)
+#print(f'원주율의 값은 {math_values[0]}이고 타입은 {type(math_values)}입니다')
+#math_values[0] = 9.99
+#print(f'원주율의 값은 {math_values[0]}이고 타입은 {type(math_values)}입니다')
+
+print(2**5)
+print(pow(2,5))
+print(divmod(9,5))
+print(type(divmod(9,5)))
+print(type(1,2))
+test = 1,2 #packing
+print(type(test))
+print(test)
+print(test[1])
+a, b, c = test #unpacking
+print(a)
+print(b)
 
 
