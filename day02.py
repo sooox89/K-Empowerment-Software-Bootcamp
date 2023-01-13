9cf4bd8e4be889bd9f98468c4a9e303dd5ff6d7e
sooox0809 <sooox89@gmail.com>
2023-01-13 15:26:07 +0900

    day02 if random

--- day02.py
+++ day02.py
@@ -18,6 +18,15 @@ if diff >= 0:
 else :
     print(f'글자수 {abs(diff)}초과')
 
+import random
+
+limits = 20
+tweets = "pass" * random.randint(1,10) # 1에서 10 사이의 정수가 임의로 발생
+diff = limits - len(tweets)
+if diff >= 0:
+    print(tweets)
+else:
+    print(f'글자수 {abs(diff)}초과')
 
 
 
