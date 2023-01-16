42b5f84e97792a7c1e7463aede1006488b1aa4e1
sooox0809 <sooox89@gmail.com>
2023-01-16 14:43:27 +0900

    ch6 prime number

--- day03_ch6.py
+++ day03_ch6.py
@@ -1,15 +1,58 @@
-#while 구구단 // continue / break /
-#for 반복문
 
-while True:
-    dan = int(input('Dan (0 to quit): '))
+#while, for문 교수님예제
+#prime number
 
-    if dan == 0 : #exit
+
+#while문 ver.1
+number = int(input("input number: "))
+counts = 0
+
+k = 1
+while k <= number:
+    if number % k == 0:
+        counts = counts + 1      # counts += 1
+    k = k + 1
+if counts == 2:
+    print('f{number} is prime number!')
+else:
+    print('f{number} is NOT prime number!')
+
+#for문 ver.2
+number = int(input("input number: "))
+counts = 0
+
+for k in range(1, number+1):
+    if number % k == 0:
+        counts = counts + 1
+if counts == 2:
+    print('f{number} is prime number!')
+else:
+    print('f{number} is NOT prime number!')
+
+
+# ver.3
+number = int(input("input number: "))
+counts = 0
+
+for k in range(1, number + 1):
+    if number % k == 0:
+        counts = counts + 1
+if counts:
+    print('f{number} is prime number!')
+else:
+    print('f{number} is NOT prime number!')
+
+# ver.4
+number = int(input("input number :"))
+is_prime = True   #변수 boolean 타입으로
+
+for k in range(2, number):
+    if number % k ==0:
+        is_prime = False
         break
-    if 1< dan < 10 : # if dan >=2 and dan <=9:
-        for i in range(1, 10):
-                print('{0} * {1} = {2}'.format(dan, i, dan * i))
-    else:
-            print('2에서 9사이의 값을 입력 하세요')
+    print(k)
 
-
+if is_prime:
+    print('f{number} is prime number!')
+else:
+    print('f{number} is NOT prime number!')
