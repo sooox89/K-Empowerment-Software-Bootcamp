717a5a555a7cbd87a6d0ea77e50686b5eb8134e1
sooox0809 <sooox89@gmail.com>
2023-01-16 14:16:41 +0900

    day03 for

--- day03_ch6.py
+++ day03_ch6.py
@@ -1,30 +1,11 @@
 #while 구구단 // continue / break /
-#while 반복문
-while True:
-    dan = int(input('Dan : '))
-
-    #if 1 < dan < 10:
-    if dan >= 2 or dan <=9:
-        i = 1
-        while i < 10:
-            print('{0} * {1} = {2}'.format(dan, i, dan * i))
-            i = i + 1
-        break
-    else:
-        print('2에서 9사이의 값을 입력 하세요')
+#for 반복문
 
 while True:
-    dan = int(input('Dan(0 to quit): '))
+    dan = int(input('Dan (0 to quit): '))
 
-    if dan == 0: # 종료
+    if dan == 0 : #exit
         break
-
-    if 1 < dan < 10:
-        i = 1
-        while i < 10:
-            print('{0} * {1} = {2}'.format(dan, i, dan * i))
-            i = i + 1
-
-    else:
-        print('2에서 9사이의 값을 입력 하세요')
-
+    if 1< dan < 10 : # if dan >=2 and dan <=9:
+        for i in range(1,10):
+            print('{0}*{1}')
