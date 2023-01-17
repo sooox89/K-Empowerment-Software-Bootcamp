d8e7763af6f6548f3e2dbe01653aa532a9889f22
sooox0809 <sooox89@gmail.com>
2023-01-17 14:49:45 +0900

    day04 ch8 dictionary2

--- day04.py
+++ day04.py
@@ -1,14 +1,45 @@
 #Ch8 dictionary
 
-students = {'name' : 'Kim inha', 'age' : 23, 'eyes' : [0.9,1.1] }
-#for k in students.keys():
-#for k in students:
-#for k in students.values():
-#   print(k)
+alcohol_foods = {
+    '맥주': '치킨',
+    '소주': '골뱅이소면',
+    '와인': '치즈',
+    '고량주': '짬뽕'
+}
+alcohol_list = list(alcohol_foods) #extract keys
+while True:
+    alcohol = input(f'술을 선택하세요 1) {alcohol_list[0]} 2) {alcohol_list[1]} 3) {alcohol_list[2]} 4) {alcohol_list[3]} 5) 계산 : ')
+    if alcohol == '5':
+        print('다음에 또 오세요~')
+        break
+    elif alcohol == '1':
+        print(f'맥주에 어울리는 안주는 {alcohol_foods["맥주"]}입니다')
+    elif alcohol == '2':
+        print(f'소주에 어울리는 안주는 {alcohol_foods["소주"]}입니다')
+    elif alcohol == '3':
+        print(f'와인에 어울리는 안주는 {alcohol_foods["와인"]}입니다')
+    elif alcohol == '4':
+        print(f'고량주에 어울리는 안주는 {alcohol_foods["고량주"]}입니다')
+    else:
+        print('')
+print(alcohol)
 
-for k, v in students.items():
-    print(f'{k} : {v}')
-print(f'이름은 {students["name"]}, 나이는 {students["age"]}', end=' ')
-print(f'시력은 좌: {students["eyes"][0]}, 우 : {students["eyes"][1]}')
 
+alcohol_list = list(alcohol_foods) #extract keys
+while True:
+    alcohol = input(f'술을 선택하세요 1) {alcohol_list[0]} 2) {alcohol_list[1]} 3) {alcohol_list[2]} 4) {alcohol_list[3]} 5) 계산 :')
+    if alcohol == '5':
+        print('다음에 또 오세요~')
+        break
+    elif alcohol == '1':
+            print(f'{alcohol_list[0]}에 어울리는 안주는 {alcohol_foods[alcohol_list[0]]}입니다')
+    elif alcohol == '2':
+            print(f'{alcohol_list[1]}에 어울리는 안주는 {alcohol_foods[alcohol_list[1]]}입니다')
+    elif alcohol == '3':
+            print(f'{alcohol_list[2]}에 어울리는 안주는 {alcohol_foods[alcohol_list[2]]}입니다')
+    elif alcohol == '4':
+            print(f'{alcohol_list[3]}에 어울리는 안주는 {alcohol_foods[alcohol_list[3]]}입니다')
+    else:
+            print('')
+    print(alcohol)
 
