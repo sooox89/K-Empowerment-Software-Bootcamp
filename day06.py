1d2140f037902064889cab1bbd7ebbe5612d4eb9
sooox0809 <sooox89@gmail.com>
2023-01-19 15:49:01 +0900

    ch10 class 포켓몬 예제

--- day06.py
+++ day06.py
@@ -1,13 +1,5 @@
 #ch 10 객체 class
 #
-# class Pokemon:
-#     def __init__(self):   # 객체 생성 시 동작
-#         print("포켓몬 객체 생성됨")
-#
-# p1 = Pokemon()   # 코드 0x1008eda90
-# p2 = Pokemon()   # 코드 0x1008edad0  // p1, p2 코드 다름
-# print(p1,p2)
-
 class Pokemon:
     def __init__(self,name,owner, skills):
         self.name = name
@@ -21,11 +13,12 @@ class Pokemon:
             print(skill)
 
 
-p1 = Pokemon('피카츄', '한지우', '50만볼트/100만볼트/번개')
-p2 = Pokemon('꼬부기', '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
-p2.info()
-p1.info()
-# print(p2.skills)
-# print(f'{p2.owner}의 포켓몬은 {p2.name}입니다')
-# print(f'{p1.owner}의 포켓몬은 {p1.name}입니다')
-# print(p1,p2)
+
+class Pikachu(Pokemon): #inheritance
+    pass
+
+pi1= Pikachu('피카츄', '덴트', '번개')
+pi1.info()    ##?
+
+p1 = Pokemon('피카츄', '한지우', '50만 볼트/100만 볼트/번개')
+p2 = Pokemon('꼬부기', '양수경', '우하하/배고파/날룸')
