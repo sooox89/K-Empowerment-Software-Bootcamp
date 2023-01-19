a79cb0be3a8017372542ad74da37847991aeb09f
sooox0809 <sooox89@gmail.com>
2023-01-19 15:39:58 +0900

    ch10 class 포켓몬 예제

--- day06.py
+++ day06.py
@@ -1,9 +1,31 @@
 #ch 10 객체 class
-# 스트링 타입으로 순회해서 ㄲ 빅뱅 리스트에서 승리만 pop
-groups = {
-    '빅뱅' : ['GD','태양','탑','대성', '승리'] , '마마무' : ['문별','솔라','휘인','화사']
-}
-for group in groups.values():
-    p
-    group[0].pop()
-    print(group)
+#
+# class Pokemon:
+#     def __init__(self):   # 객체 생성 시 동작
+#         print("포켓몬 객체 생성됨")
+#
+# p1 = Pokemon()   # 코드 0x1008eda90
+# p2 = Pokemon()   # 코드 0x1008edad0  // p1, p2 코드 다름
+# print(p1,p2)
+
+class Pokemon:
+    def __init__(self,name,owner, skills):
+        self.name = name
+        self.owner = owner
+        self.skills = skills.split('/')     ## 리스트 split !
+        print(f"포켓몬 {name} 생성됨")
+
+    def info(self):
+        print(f'{self.owner}의 포켓몬은 {self.name}입니다')    #실행 시점의 클래스 실행
+        for skill in self.skills:
+            print(skill)
+
+
+p1 = Pokemon('피카츄', '한지우', '50만볼트/100만볼트/번개')
+p2 = Pokemon('꼬부기', '오바람', '고속스핀/거품/몸통박치기/하이드로펌프')
+p2.info()
+p1.info()
+# print(p2.skills)
+# print(f'{p2.owner}의 포켓몬은 {p2.name}입니다')
+# print(f'{p1.owner}의 포켓몬은 {p1.name}입니다')
+# print(p1,p2)
