bcd91536902f6d272b895b9ce9b05507ea2ff2ba
sooox0809 <skycasey@inha.edu>
2023-02-03 13:24:41 +0900

    day 15 liner list 04 / code 03-04

--- day15.py
+++ day15.py
@@ -1,6 +1,12 @@
-# self study
+# code 03-04
 
 def insert_data(idx, pokemon):
+    """
+    선형 리스트의 idx 위치에 원소 삽입
+    :param idx:
+    :param pokemon:
+    :return:
+    """
     if idx < 0 or idx > len(pokemons):
         print("out of range !")
         return
@@ -15,6 +21,11 @@ def insert_data(idx, pokemon):
 
 
 def delete_data(idx):
+    """
+    선형 리스트 idx 위치의 원소 삭제
+    :param idx: int
+    :return: void
+    """
     if idx < 0 or idx > len(pokemons):
         print("데이터를 삭제할 범위를 벗어났습니다.")
         return
@@ -40,22 +51,62 @@ def delete_data(idx):
     #     pokemons.pop()
 
 
+def add_data(pokemon):
+    """
+    선형 리스트
+    :param pokemon:
+    :return:
+    """
+    pokemons.append(None)
+    pokemons[len(pokemons)-1] = pokemon
+
+pokemons = []
+menu = -1
+
+if __name__ == "__main__":   ## 메인 코드 부분 ##
+
+    while menu != 4:
+
+        menu = int(input("선택하세요(1: 추가, 2: 삽입, 3: 삭제, 4: 종료)--> "))
+
+        if (menu == 1):
+                data = input("추가할 데이터--> ")
+                add_data(data)
+                print(pokemons)
+        elif (menu == 2):
+                pos = int(input("삽입할 위치--> "))
+                data = input("추가할 데이터--> ")
+                insert_data(pos, data)
+                print(pokemons)
+        elif (menu == 3):
+                pos = int(input("삭제할 위치--> "))
+                delete_data(pos)
+                print(pokemons)
+        elif (menu == 4):
+                print(pokemons)
+                exit()
+                print("menu에서 고르세요")
+                continue
 
 
 
-if __name__ == "__main__":
-    pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
-    # insert_data(2, '거북왕')
-    # pokemons.insert(2,'거북왕')
-    delete_data(1)
-    print(pokemons)
-    # insert_data(6, '어니부기')
-    # pokemons.insert(6,'어니부기')
-    # delete_data(3)
+
+
+
+
     #
+    # # insert_data(2, '거북왕')
+    # # pokemons.insert(2,'거북왕')
+    # delete_data(1)
     # print(pokemons)
-
-
+    # # insert_data(6, '어니부기')
+    # # pokemons.insert(6,'어니부기')
+    # delete_data(3)
+    # print(pokemons)
+    # add_data('터검니')
+    # print(pokemons)
+    #
+    #
 
 
 
