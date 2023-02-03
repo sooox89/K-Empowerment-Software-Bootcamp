c9943af90ec797f57daeaa063b6fa9f1f3f5b5b9
sooox0809 <skycasey@inha.edu>
2023-02-03 15:49:13 +0900

    day 15 simple linked list 01 / code 04-05

--- day15.py
+++ day15.py
@@ -1,41 +1,42 @@
-# 응용예제 2
-## 함수 선언 부분 ##
-def printPoly(px):
-	poly_str = "P(x) = "
-
-	for i in range(len(px[0])):
-		term = px[0][i]	# 항 차수
-		coef = px[1][i]	# 계수
-
-		if (coef >= 0):
-			poly_str += "+"
-		poly_str += str(coef) + "x^" + str(term) + " "
-
-	return poly_str
+# code 04-05
+# simple linked list
+## 클래스와 함수 선언 부분 ##
+class Node():
+	def __init__(self):
+		self.data = None
+		self.link = None
 
 
-def calcPoly(xVal, p_x):
-	ret_value = 0
-
-	for i in range(len(p_x[0])):
-		term = p_x[0][i]	# 항 차수
-		coef = p_x[1][i]	# 계수
-		ret_value += coef * xValue ** term
-		term -= 1
-
-	return ret_value
+def print_nodes(start):
+	current = start
+	if current == None :
+		return
+	print(current.data, end=' ')
+	while current.link != None:
+		current = current.link   # 다음 노드로 이동
+		print(current.data, end=' ')
+	print()
 
 
 ## 전역 변수 선언 부분 ##
-px = [ [300, 20, 0],
-		[7, -4, 5] ]
+memory = []
+head, current, pre = None, None, None
+data_array = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+
 
 ## 메인 코드 부분 ##
-if __name__ == "__main__":
-	pStr = printPoly(px)
-	print(pStr)
+if __name__ == "__main__" :
 
-	xValue = int(input("X 값-->"))
+	node = Node()		# 첫 번째 노드
+	node.data = data_array[0]
+	head = node
+	memory.append(node)
 
-	pxValue = calcPoly(xValue, px)
-	print(pxValue)
+	for data in data_array[1:] :	# 두 번째 이후 노드
+		pre = node
+		node = Node()
+		node.data = data
+		pre.link = node    # 이전 링크를 노드에 연결
+		memory.append(node)
+
+	print_nodes(head)
