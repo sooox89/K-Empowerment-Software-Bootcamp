451ca3d96cf26df5e564a877cf2920b62c28e5e0
sooox0809 <skycasey@inha.edu>
2023-02-03 11:30:25 +0900

    day 15 liner list 03 / code 03-03

--- day15.py
+++ day15.py
@@ -13,11 +13,31 @@ def insert_data(idx, pokemon):
 
     pokemons[idx] = pokemon  # 지정한 위치에 친구 추가
 
+
+def delete_data(idx):
+    if idx < 0 or idx > len(pokemons):
+        print("데이터를 삭제할 범위를 벗어났습니다.")
+        return
+
+    len_pokemons = len(pokemons)
+    pokemons[idx] = None  # 데이터 삭제
+
+    for i in range(idx + 1, len_pokemons):
+        pokemons[i - 1] = pokemons[i]
+        pokemons[i] = None  # 배열의 맨 마지막 위치 삭제
+
+    del (pokemons[len_pokemons - 1])
+
+
 if __name__ == "__main__":
     pokemons = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
     # insert_data(2, '거북왕')
-    pokemons.insert(2,'거북왕')
+    # pokemons.insert(2,'거북왕')
+    delete_data(1)
     print(pokemons)
     # insert_data(6, '어니부기')
-    pokemons.insert(6,'어니부기')
+    # pokemons.insert(6,'어니부기')
+    delete_data(3)
+
     print(pokemons)
+
