2c1f3f78ea87fdac9efdbf5242a9f2655cf7e8aa
sooox0809 <skycasey@inha.edu>
2023-02-03 14:25:09 +0900

    day 15 liner list 07 / code 03-06

--- day15.py
+++ day15.py
@@ -1,55 +1,47 @@
-# code 03-05
-
-def print_poly(px):
+# code 03-06
+def print_poly(px, tx):
     """
     다항식을 포맷에 맞게 출력하는 함수
     :param px: 계수를 원소로 가지고 있는 list
-    :return px : 다항식 문자열
+    :param tx: 차수를 원소로 가지고 있는 list
+    :return: 다항식 문자열
     """
-    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
     poly_str = "P(x) = "
 
     for i in range(len(px)):
-        coef = px[i]  # 계수
-        if i > 0 and  coef > 0:
+        term = tx[i]
+        coef = px[i]
+        if coef >= 0:
             poly_str = poly_str + "+"
 
-        elif coef == 0:
-            term = term - 1
-            continue
         poly_str = poly_str + f'{coef}x^{term} '
-        term = term - 1
+
+    return poly_str
 
 
-    return  poly_str
-
-
-def cal_ploy(x_val, px):
+def calc_poly(x_val, px, tx):
     """
     다항식의 산술연산을 하는 함수
-    :param x_val: x 값 integer
+    :param x_val: x값 integer
     :param px: 계수를 원소로 가지고 있는 list
+    :param tx: 차수를 원소로 가지고 있는 list
     :return: 다항식 계산 결과 값 integer
     """
-    ret_value = 0
-    term = len(px) - 1  # 최고차항 숫자 = 배열길이-1
+    return_val = 0
 
     for i in range(len(px)):
-        coef = px[i]  # 계수
-        ret_value += coef * x_value ** term
-        term = term - 1
+        term = tx[i]
+        coef = px[i]
+        return_val = return_val + coef * x_val ** term
 
-    return ret_value
+    return return_val
 
 
-## 전역 변수 선언 부분 ##
-px = [3, -4, 0, 6]  # = 3x^3 -4x^2 +0x^1 +6x^0
+px = [3, -4, 5]
+tx = [300, 20, 0]
 
-## 메인 코드 부분 ##
 if __name__ == "__main__":
-    print(print_poly(px))
+    print(print_poly(px, tx))
 
-    x_value = int(input("X값 : "))
-
-    print(cal_ploy(x_value, px))
-
+    x_value = int(input("X 값 : "))
+    print(calc_poly(x_value, px, tx))
