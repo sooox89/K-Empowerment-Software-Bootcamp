a246fb2ba9b5654dfeedad966d33276512a12d93
sooox0809 <skycasey@inha.edu>
2023-02-06 15:37:47 +0900

    day 16 stack 02  code 06- 08

--- day16.py
+++ day16.py
@@ -1,18 +1,63 @@
-stack = [ None for _ in range(5)]
-top= -1
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE-1:
+        return True
+    else:
+        return False
+def is_stack_empty():
+    global SIZE,stack,top
+    if top == -1:
+        return  True
+    return False
 
-top = top +1
-stack[top] = "커피"
-top = top +1
-stack[top] = "녹차"
-top = top + 1
-stack[top] = "꿀물"
+def push(data):
+    global SIZE, stack, top
+    if is_stack_full():
+        print("Stack is full")
+        return
+    top = top +1
+    stack[top] =data
 
-for i in range(len(stack)-1,-1,-1):
-    print(stack[i])
+def pop():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is empty")
+        return
+    temp = stack[top]
+    stack[top] = None
+    top = top -1
+    return temp
+def peek():
+    global SIZE, stack, top
+    if is_stack_empty():
+        print("Stack is empty")
+        return None
+    return stack[top]
 
-data = stack[top]
-stack[top] = None
-top = top -1
-print(data)
+SIZE = int(input("Stack Size : "))
+stack = [None for _ in range(SIZE)]    ##저장공간
+top = -1
+
+if __name__ == "__main__":
+    while True:
+        menu = input("삽입(I)/추출(E)/확인(V)/종료(X) 중 하나를 선택 ==> ")
+        if  menu == 'X' or menu == 'x':
+            break
+        elif menu == 'I' or menu == 'i':
+            data = input("입력할 데이터 ==> ")
+            push(data)
+            print("스택 상태 : ", stack)
+        elif menu == 'E' or menu == 'e':
+            data = pop()
+            print("추출된 데이터 ==> ", data)
+            print("스택 상태 : ", stack)
+        elif menu == 'V' or menu == 'v':
+            data = peek()
+            print("확인된 데이터 ==> ", data)
+            print("스택 상태 : ", stack)
+        else:
+            print("입력이 잘못됨")
+
+
+    print("프로그램 종료!")
 
