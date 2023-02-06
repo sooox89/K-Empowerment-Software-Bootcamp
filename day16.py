30930cfe6d00d1882ff9731d281484f0aee70a5f
sooox0809 <skycasey@inha.edu>
2023-02-06 14:10:22 +0900

    day 16 linked list 05

--- day16.py
+++ day16.py
@@ -1,3 +1,4 @@
+import random
 
 
 class Node:
@@ -11,7 +12,7 @@ def print_nodes(start):
     if current == None :
         return
     print(current.data, end=' ')
-    while current.link != None:
+    while current.link != start:  #
         current = current.link
         print(current.data, end=' ')
     print()
@@ -22,11 +23,15 @@ def insert_nodes(find_data, insert_data):
     if head.data == find_data:
         node = Node(insert_data)
         node.link = head
+        last = head
+        while last.link != head:
+            last = last.link
+        last.link = node
         head = node
         return
 
     current = head
-    while current.link != None:
+    while current.link != head:
         pre = current
         current = current.link
         if current.data == find_data:
@@ -37,29 +42,30 @@ def insert_nodes(find_data, insert_data):
 
     node = Node(insert_data)
     current.link = node
+    node.link = head
 
 
 def delete_nodes(delete_data):
     global head, current, pre
 
     if head.data == delete_data:
-        print('첫 번째 노드 삭제 완료')
         current = head
         head = head.link
+        last = head  #
+        while last.link != current:  #
+            last = last.link  #
+        last.link = head  #
         del current
         return
 
     current = head
-    while current.link != None:
+    while current.link != head:
         pre = current
         current = current.link
         if current.data == delete_data:
-            print('중간 노드 삭제 완료')
             pre.link = current.link
             del current
             return
-
-    print('삭제할 노드를 찾지 못함')
 
 
 def find_nodes(find_data):
@@ -69,7 +75,7 @@ def find_nodes(find_data):
     if current.data == find_data:
         return current
 
-    while current.link != None:
+    while current.link != head:  #
         current = current.link
         if current.data == find_data:
             return current
@@ -77,31 +83,83 @@ def find_nodes(find_data):
     return Node(None)
 
 
-head, current, pre = None, None, None
-data_array = ["피카츄", "라이츄", "꼬부기", "파이리", "이상해"]
+def is_find(find_data):
+    """
+    연결 리스트안에서 원소 존재 여부 판정 함수
+    :param find_data: 찾고자 하는 원소. str
+    :return: 연결 리스트안에서 원소가 존재하면 True리턴 아니면 False
+    """
 
+    global head, current, pre
+
+    current = head
+    if current.data == find_data:
+        return True
+
+    while current.link != head:  #
+        current = current.link
+        if current.data == find_data:
+            return True
+
+    return False
+
+
+def count_odd_even():
+    global head, current
+
+    even, odd = 0, 0
+
+    # SRP 위배
+    # if head == None:
+    #     return False
+
+    current = head
+    while True:
+        if current.data % 2 == 0:
+            even = even + 1
+        else:
+            odd = odd + 1
+        if current.link == head:
+            break
+        current = current.link
+
+    return odd, even
+
+
+def makeSquareNumber(odd, even):
+    if odd > even:
+        remainder = 1
+    else:
+        remainder = 0
+
+    current = head
+    while current.link != head:
+        if current.data % 2 == remainder:
+            current.data = current.data * current.data
+        current = current.link
+    current.data = current.data * current.data
+
+
+head, current, pre = None, None, None
+data_array = list()
 
 if __name__ == "__main__":
+    # odd_even = count_odd_even()  # False 리턴
+    for _ in range(7):
+        data_array.append(random.randint(1, 10))
+
     node = Node(data_array[0])
     head = node
+    node.link = head
 
     for data in data_array[1:]:
         pre = node
         node = Node(data)
         pre.link = node
+        node.link = head
 
     print_nodes(head)
-    insert_nodes("피카츄", "잠만보")
+    odd_even = count_odd_even()
+    print(f'Odd Number : {odd_even[0]}, Even Number {odd_even[1]}')
+    makeSquareNumber(odd_even[0], odd_even[1])
     print_nodes(head)
-    insert_nodes("파이리", "어니부기")
-    print_nodes(head)
-    insert_nodes("성윤모", "거북왕")
-    print_nodes(head)
-    delete_nodes("잠만보")
-    print_nodes(head)
-    delete_nodes("어니부기")
-    print_nodes(head)
-    delete_nodes("강찬석")
-    print_nodes(head)
-    print(find_nodes("파이리").data)
-    print(find_nodes("박민석").data)
