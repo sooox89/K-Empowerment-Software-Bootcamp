c16fd723786e9e5e431ae56a7421ec9a027e2004
sooox0809 <skycasey@inha.edu>
2023-02-08 14:31:04 +0900

    day18 CH9 그래프 깊이 우선 탐색 abcdefghi 알고리즘 오류있음 !

--- day18.py
+++ day18.py
@@ -1,5 +1,3 @@
-from collections import deque
-
 ## 클래스 및 함수 선언 부분 ##
 class Graph() :
 	def __init__ (self, size) :
@@ -8,32 +6,36 @@ class Graph() :
 
 ## 전역 변수 선언 부분 ##
 G1 = None
-# queue = []
-queue = deque([])   #dequeue as queue
-visitedAry = []		# 방문한 정점
+stack = [] 			# append() as push() and built in pop()
+visited_array = []		# 방문한 정점
 
 ## 메인 코드 부분 ##
-G1 = Graph(4)
-G1.graph[0][2] = 1; G1.graph[0][3] = 1
-G1.graph[1][2] = 1
-G1.graph[2][0] = 1; G1.graph[2][1] = 1; G1.graph[2][3] = 1
-G1.graph[3][0] = 1; G1.graph[3][2] = 1
-
+G1 = Graph(9)
+A,B,C,D,E,F,G,H,I = 0,1,2,3,4,5,6,7,8
+G1.graph[A][B] = 1; G1.graph[A][C] = 1; G1.graph[A][E] = 1
+G1.graph[B][A] = 1; G1.graph[B][C] = 1; G1.graph[B][D] = 1
+G1.graph[C][A] = 1; G1.graph[C][B] = 1; G1.graph[C][D] = 1; G1.graph[C][E] = 1; G1.graph[C][F] = 1
+G1.graph[D][B] = 1; G1.graph[D][C] = 1
+G1.graph[E][A] = 1; G1.graph[E][C] = 1; G1.graph[E][F] = 1; G1.graph[E][H] = 1
+G1.graph[F][C] = 1
+G1.graph[G][D] = 1; G1.graph[G][I] = 1
+G1.graph[H][D] = 1; G1.graph[H][I] = 1
+G1.graph[I][F] = 1; G1.graph[I][H] = 1
 print('## G1 무방향 그래프 ##')
-for row in range(4) :
-	for col in range(4) :
+for row in range(9) :
+	for col in range(9) :
 		print(G1.graph[row][col], end = ' ')
 	print()
 
 current = 0		# 시작 정점 A
-queue.append(current)
-visitedAry.append(current)
+stack.append(current)
+visited_array.append(current)
 
-while (len(queue) != 0) :
+while (len(stack) != 0) :
 	next = None
-	for vertex in range(4) :
+	for vertex in range(9) :
 		if G1.graph[current][vertex] == 1 :
-			if vertex in visitedAry :  	   # 방문한 적이 있는 정점이면 탈락
+			if vertex in visited_array :  	   # 방문한 적이 있는 정점이면 탈락
 				pass
 			else : 			   # 방문한 적이 없으면 다음 정점으로 지정
 				next = vertex
@@ -41,14 +43,12 @@ while (len(queue) != 0) :
 
 	if next != None :			  	   # 다음에 방문할 정점이 있는 경우
 		current = next
-		queue.append(current)
-		visitedAry.append(current)
+		stack.append(current)
+		visited_array.append(current)
 	else :            	  	  	  	  # 다음에 방문할 정점이 없는 경우
-		# current = queue.pop(0)       # O(n) OVERHEAD 발생
-		current = queue.popleft()	   # O(1) dequeue
+		current = stack.pop()
 
 
 print('방문 순서 -->', end='')
-for i in visitedAry :
-	print(i, end=' ---> ')
-print("END")
+for i in visited_array :
+	print(chr(ord('A')+i), end='   ')
