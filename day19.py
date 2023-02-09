0a805de8adf437457b170cb10072feea0836cb6a
sooox0809 <skycasey@inha.edu>
2023-02-09 14:25:44 +0900

    day19 CH12 self study code 12-2

--- day19.py
+++ day19.py
@@ -1,25 +1,30 @@
 import random
 
 ## 함수 선언 부분 ##
-def find_insert_idx(ary, data) :
-	find_idx = -1			# 초깃값은 없는 위치로
-	for i in range(0, len(ary)) :
-		if (ary[i] < data ) :
-			find_idx = i
-			break
-	if find_idx == -1 :			# 큰 값을 못찾음 == 제일 마지막 위치
-		return len(ary)
-	else :
-		return find_idx
+def quickSort(ary):
+    global count
+    n = len(ary)
+    if n <= 1:  # 정렬할 리스트의 개수가 1개 이하면
+        return ary
+
+    pivot = ary[n // 2]  # 기준값을 중간값으로 지정
+    leftAry, rightAry = [], []
+
+    for num in ary:
+        count += 1
+        if num > pivot:
+            leftAry.append(num)
+        elif num < pivot:
+            rightAry.append(num)
+
+    return quickSort(leftAry) + [pivot] + quickSort(rightAry)
+
 
 ## 전역 변수 선언 부분 ##
-before = [random.randint(0,200) for  _  in range (10)]
-after = []
-
+dataAry = [random.randint(0,200)for _ in range(20)]
+count=0
 ## 메인 코드 부분 ##
-print('정렬 전 -->', before)
-for i in range(len(before)) :
-	data = before[i]
-	insert_position = find_insert_idx(after, data)
-	after.insert(insert_position, data)
-print('정렬 후 -->', after)
+print('정렬 전 -->', dataAry)
+dataAry = quickSort(dataAry)
+print('정렬 후 -->', dataAry)
+print("##",count,"회로 정렬 완료")
